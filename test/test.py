13a089920bc312f4ce7775e0ac9ce24cd8f8befd
njcx <njcx86@gmail.com>
2020-04-24 12:43:29 +0800

    init

---
+++ test/test.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Author  : nJcx
+# @Email   : njcx86@gmail.com
+
+
+from elasticsearch import Elasticsearch
+from settings import es_ip, es_port
+import xmltodict
+es = Elasticsearch([{'host': es_ip, 'port': es_port}])
+
+
+def xml_to_json(path):
+    try:
+        with open(path, 'r') as load_f:
+            temp_ = xmltodict.parse(load_f).get("nmaprun")
+            return {key: temp_[key]
+                    for key in temp_
+                    if key not in ["verbose", 'scaninfo', 'taskbegin', 'taskend', "debugging"]}
+
+    except Exception as e:
+        print(e)
+        return {}
+
+
+def json_to_es(index, json_):
+    try:
+        es.index(index=index, doc_type="vuln", body=json_)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    json_ = xml_to_json('test.xml')
+    json_to_es('test',json_)
+
+    # print(json_)
+
