ade4508ed18f99869cf14febc0deb67b3a646c0a
njcx <njcx86@gmail.com>
2020-04-24 17:41:16 +0800

    init

--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@
 
 from elasticsearch import Elasticsearch
 from settings import es_ip, es_port
-import xmltodict
+import xmltodict , json
 es = Elasticsearch([{'host': es_ip, 'port': es_port}])
 
 
@@ -24,14 +24,16 @@ def xml_to_json(path):
 
 def json_to_es(index, json_):
     try:
-        es.index(index=index, doc_type="vuln", body=json_)
+
+        print json.dumps(json_.get('host').get('ports').get('port'))
+        es.index(index=index, doc_type="vuln", body=json.dumps(json_))
     except Exception as e:
         print(e)
 
 
 if __name__ == '__main__':
     json_ = xml_to_json('test.xml')
-    json_to_es('test',json_)
+    json_to_es('test', json_)
 
     # print(json_)
 
